0ed35552fc5fa4600c9fe05b2cf665d3c1b5430e
ecalimac <elena.calimac@gmail.com>
2020-06-21 19:19:31 +0300

    install Semantic UI and test it

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Container } from "semantic-ui-react";
+import Login from "./Login";
+
+const App = () => (
+  <Container>
+    <Login />
+  </Container>
+);
+
+export default App;
